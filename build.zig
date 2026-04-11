a019ea6103e0641774d7445c81a67fa2925b49cd
MorgenGramlich <danila.sergeyev@gmail.com>
2026-04-10 20:25:09 -0500

    Update build script

--- build.zig
+++ build.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const version_str = b.option([]const u8, "version", "The release version") orelse "dev";
+
     const exe = b.addExecutable(.{
         .name = "sklient",
         .root_module = b.createModule(.{
@@ -30,4 +32,32 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
+
+    const release_step = b.step("release", "Build for all release targets");
+    const targets: []const std.Target.Query = &.{
+        .{ .cpu_arch = .aarch64, .os_tag = .macos },
+        .{ .cpu_arch = .aarch64, .os_tag = .linux },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux },
+    };
+
+    for (targets) |t| {
+        const resolved_target = b.resolveTargetQuery(t);
+        const target_name = b.fmt("sklient-{s}-{s}-{s}", .{
+            version_str,
+            @tagName(t.cpu_arch.?),
+            @tagName(t.os_tag.?),
+        });
+
+        const target_exe = b.addExecutable(.{
+            .name = target_name,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = resolved_target,
+                .optimize = .ReleaseSafe,
+            }),
+        });
+
+        const target_install = b.addInstallArtifact(target_exe, .{});
+        release_step.dependOn(&target_install.step);
+    }
 }
